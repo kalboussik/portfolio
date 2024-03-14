153433d28e014dc7d5c589724be6b0c5c28c87df
kalboussik <63822767+kalboussik@users.noreply.github.com>
2024-03-15 00:52:10 +0100

    up

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,8 +84,12 @@ const filterFunc = function (selectedValue) {
 
     if (selectedValue === "all") {
       filterItems[i].classList.add("active");
-    } else if (selectedValue === filterItems[i].dataset.category) {
-      filterItems[i].classList.add("active");
+    } else if (selectedValue === "Freelance") {
+      filterItems[1].classList.add("active");
+    } else if (selectedValue === "Enseignement et Encadrement") {
+      filterItems[2].classList.add("active");
+    } else if (selectedValue === "Recherche scientifique") {
+      filterItems[3].classList.add("active");
     } else {
       filterItems[i].classList.remove("active");
     }
